e2fad0375b1b0fb7a9eacc7c6beff997f72b9351
Roman Hyde <romanhyde1@gmail.com>
2022-06-24 16:58:14 +0100

    add collection page with product card component

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -8,7 +8,7 @@ import {
     useServerAnalytics,
 } from "@shopify/hydrogen";
 import { Suspense } from "react";
-
+import ProductCard from "../../components/ProductCard.server";
 import { Layout } from "../../components/Layout.server";
 
 export default function Collection() {
@@ -50,6 +50,14 @@ export default function Collection() {
                     </div>
                 )}
             </header>
+
+            <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+                <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {collection.products.nodes.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            </section>
         </Layout>
     );
 }
@@ -65,6 +73,40 @@ const QUERY = gql`
         seo {
             description
             title
+        }
+        image {
+            id
+            url
+            width
+            height
+            altText
+        }
+        products(first: 8) {
+            nodes {
+                id
+                title
+                publishedAt
+                handle
+                variants(first:1) {
+                    nodes {
+                        id
+                        image {
+                            url
+                            altText
+                            width
+                            height
+                        }
+                        priceV2 {
+                            amount
+                            currencyCode
+                        }
+                        compareAtPriceV2{
+                            amount
+                            currencyCode
+                        }
+                    }
+                }
+            }
         }
     }
   }
