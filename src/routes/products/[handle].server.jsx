904ab69d5281618ae87a771f950849bef2976472
Roman Hyde <romanhyde1@gmail.com>
2022-06-24 18:50:01 +0100

    Add product handle query

--- src/routes/products/[handle].server.jsx
+++ src/routes/products/[handle].server.jsx
@@ -1,15 +1,54 @@
 import {
+    gql,
+    useShopQuery,
+    useServerAnalytics,
     useRouteParams,
+    ShopifyAnalyticsConstants,
+    Seo,
   } from "@shopify/hydrogen";
+  import { Suspense } from "react";
   import { Layout } from "../../components/Layout.server";
-  export default function Product() {
+  export default function Product({ params }) {
     const { handle } = useRouteParams();
+
+    const{
+        data: { product },
+    } = useShopQuery({
+        query: PRODUCT_QUERY,
+        variables: {
+            handle,
+        },
+    });
+
+    useServerAnalytics({
+        shopify:{
+            pageType: ShopifyAnalyticsConstants.pageType.product,
+            resourceId: product.id,
+        },
+    });
 
     return (
       <Layout>
+        <Suspense>
+            <Seo type="product" data={product} />
+        </Suspense>
         <section className="p-6 md:p-8 lg:p-12">
-          This will be the product page for <strong>{handle}</strong>
+          This will be the product page for <strong>{product.title}</strong>
         </section>
       </Layout>
     );
   }
+
+  const PRODUCT_QUERY = gql`
+    query Product($language: LanguageCode, $handle: String!)
+    @inContext(language: $language) {
+        product(handle: $handle) {
+            id
+            title
+            seo {
+                title
+                description
+            }
+        }
+    }
+  `;
